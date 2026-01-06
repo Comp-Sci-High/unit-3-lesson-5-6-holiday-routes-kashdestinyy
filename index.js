156b26684c7a717d0d01c376c9233aad1148f952
kashdestinyy <destiny.francis26@compscihigh.org>
2026-01-06 02:30:25 +0000

    Done

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
+//import Express browser to create a web server
 const express = require("express")
+//creating an  Express application instance
 const app = express()
 
+//serve all static files = allowing browser to acess files like index.html
 app.use(express.static(__dirname + "/public"))
 
+//handles get request for the root url "/" = when someone visits the homepage it sends them index.html
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
 })
 
+
+//handles the get request to "/valentine" = when someone visits /valentine, it will send them valentine.html 
 app.get("/valentine", (req, res) => {
   res.sendFile(__dirname + "/public/valentine.html");
 })
 
+// starts server on port 3000
+// allows the server to listen for incoming request
 app.listen(3000, () => {
   console.log(`Holiday Server is Running!`)
 });
